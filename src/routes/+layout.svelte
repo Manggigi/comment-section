38327c0279bf476e6d46058baa151cfc87f63254
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-20 23:58:46 +0800

    style: Add layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import '../app.css';
+
+	export let data: LayoutData;
+</script>
+
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+	<main class="mx-auto max-w-3xl">
+		<slot />
+	</main>
+</div>
