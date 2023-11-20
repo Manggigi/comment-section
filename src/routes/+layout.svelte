e4995dd1b181e8da2c806516a51db0ab32b43201
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-21 01:01:15 +0800

    style: Update layout background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,10 @@
 	export let data: LayoutData;
 </script>
 
-<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-	<main class="mx-auto max-w-3xl">
-		<slot />
-	</main>
+<div class="bg-indigo-50">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<main class="mx-auto max-w-3xl">
+			<slot />
+		</main>
+	</div>
 </div>
